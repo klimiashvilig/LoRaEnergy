1cc7573e87a0c0ab56cc8fcdc5262a462a631942
klimiashvilig <40772717+klimiashvilig@users.noreply.github.com>
2019-02-07 11:44:36 -0500

    Update lora-energy-model-helper.cc

--- lora-energy-model-helper.cc
+++ lora-energy-model-helper.cc
@@ -1,23 +1,3 @@
-/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
-/*
- * Copyright (c) 2010 Network Security Lab, University of Washington, Seattle.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation;
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *
- * Authors: Sidharth Nabar <snabar@uw.edu>, He Wu <mdzz@u.washington.edu>
- */
-
 #include "lora-energy-model-helper.h"
 #include "ns3/lora-net-device.h"
 //#include "ns3/wifi-tx-current-model.h"
